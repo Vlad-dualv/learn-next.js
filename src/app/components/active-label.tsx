dbc21ae5ce8ef62c681d15162198a4c0d8d8368c
Vlad <vladswork95@gmail.com>
2025-08-16 12:00:18 +0100

    css modules

---
+++ src/app/components/active-label.tsx
@@ -0,0 +1,9 @@
+import css from './active-label.module.css';
+
+export interface ActiveLabelProps {
+  children: React.ReactNode;
+}
+
+export default function ActiveLabel({ children }: ActiveLabelProps) {
+  return <span className={css.label}>{children}</span>;
+}
---
+++ src/app/components/not-active-label.tsx
@@ -0,0 +1,9 @@
+import css from './not-active-label.module.css';
+
+export interface NotActiveLabelProps {
+  children: React.ReactNode;
+}
+
+export default function ActiveLabel({ children }: NotActiveLabelProps) {
+  return <span className={css.label}>{children}</span>;
+}
