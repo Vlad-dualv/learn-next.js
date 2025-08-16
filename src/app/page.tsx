316c8a235bc3662c06dc51ad7e422da710fedc06
Vlad <vladswork95@gmail.com>
2025-08-16 15:15:11 +0100

    client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { headers } from 'next/headers';
 import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
+  console.log(headers());
   return (
     <main>
-      <h1 className="text-2xl">Home Page</h1>
+      <h1 className="text-2xl">{new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
