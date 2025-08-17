a7ac670a164bdf48ebce08fe8c073f6190b8edf9
Vlad <vladswork95@gmail.com>
2025-08-17 16:57:41 +0100

    error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { headers } from 'next/headers';
 import AddCompanyButton from './components/add-company-button';
-import ServerComponent from './components/server-component';
-import ClientComponent from './components/client-component';
+import MagicButton from './components/magic-button';
 
 export default function Home() {
   console.log(headers());
@@ -9,6 +8,7 @@ export default function Home() {
     <main>
       <h1 className="text-2xl">{new Date().toTimeString()}</h1>
       <AddCompanyButton />
+      <MagicButton />
     </main>
   );
 }
